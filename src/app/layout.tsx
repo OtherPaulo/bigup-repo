54d6bba3bd8434f66686b38f793a880135d69337
Paulo Rodrigues <paulovinicius356@gmail.com>
2024-10-25 22:27:07 -0300

    adição do google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GoogleTagManager from "@/components/custom/googleTagManager";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,7 +36,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
+      <head>
+        {/* <GoogleTagManager containerId="GTM-T4DLRNBG" /> */}
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
