9e7f80d4f87d54d86780adcda80f454a83d721a7
Paulo Rodrigues <paulovinicius356@gmail.com>
2024-10-26 19:39:56 -0300

    create page blog

---
+++ src/app/(blog)/blog/_components/blog.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/(LandingPage)/footer/_components/footer";
+import Header from "@/components/(LandingPage)/header/_components/header";
+
+export default function Blog() {
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+  )
+}
---
+++ src/app/(blog)/blog/page.tsx
@@ -0,0 +1,5 @@
+import Blog from "./_components/blog";
+
+export default function Page() {
+    return <Blog />
+}
