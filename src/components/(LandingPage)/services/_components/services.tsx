e6219af3b1ecf3ed038b5d8a0690b0cbbb7b616a
Paulo Rodrigues <paulovinicius356@gmail.com>
2024-10-30 20:22:38 -0300

    att text color services

--- src/components/(LandingPage)/services/_components/services.tsx
+++ src/components/(LandingPage)/services/_components/services.tsx
@@ -3,22 +3,22 @@ import { Award, ShoppingCart, Play, Headset } from 'lucide-react'
 export default function ServicesSection() {
   const services = [
     {
-      icon: <Award className="w-12 h-12 text-purple-500" />,
+      icon: <Award className="w-12 h-12 text-[#AEFF00]" />,
       title: "BRANDING",
       description: "Nosso serviço de branding oferece uma abordagem completa para ajudar sua empresa a criar uma identidade única e memorável."
     },
     {
-      icon: <ShoppingCart className="w-12 h-12 text-purple-500" />,
+      icon: <ShoppingCart className="w-12 h-12 text-[#AEFF00]" />,
       title: "E–COMMERCE",
       description: "Nossos serviços de e-commerce são projetados para ajudar sua empresa a construir e otimizar sua loja virtual, proporcionando uma experiência de compra excepcional para seus clientes."
     },
     {
-      icon: <Play className="w-12 h-12 text-purple-500" />,
+      icon: <Play className="w-12 h-12 text-[#AEFF00]" />,
       title: "SOCIAL MEDIA",
       description: "Nossos serviços de social media são projetados para ajudar sua empresa a construir uma comunidade engajada, fortalecer sua imagem e impulsionar suas vendas."
     },
     {
-      icon: <Headset className="w-12 h-12 text-purple-500" />,
+      icon: <Headset className="w-12 h-12 text-[#AEFF00]" />,
       title: "Suporte e Consultoria",
       description: "Nossos serviços de suporte e consultoria em marketing oferecem a expertise necessária para ajudar sua empresa a alcançar seus objetivos e se destacar no mercado."
     }
@@ -28,8 +28,8 @@ export default function ServicesSection() {
     <section className="py-16 bg-gray-100">
       <div className="container mx-auto px-4">
         <div className="text-left mb-8">
-          <h2 className="text-sm font-semibold text-purple-600 mb-2">SERVIÇOS</h2>
-          <h3 className="text-3xl font-bold text-gray-900">
+        <div className="inline-block bg-[#AEFF00]/60 text-black text-sm font-medium px-3 py-1 rounded-full mb-8">Serviços</div>
+        <h3 className="text-3xl font-bold text-gray-900">
             Veja como podemos apoiar sua empresa
           </h3>
           </div>
@@ -37,7 +37,7 @@ export default function ServicesSection() {
           {services.map((service, index) => (
             <div key={index} className="bg-white p-6 rounded-lg shadow-lg relative pt-16">
               <div className="absolute -top-6 left-1/2 transform -translate-x-1/2">
-                <div className="bg-purple-200 p-4 rounded-full shadow-lg">
+                <div className="bg-[#000000]/95 p-4 rounded-full shadow-lg">
                   {service.icon}
                 </div>
               </div>
