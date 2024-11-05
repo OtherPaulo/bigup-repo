bece45dc27fabf7fa58753dbc07e7e8f4507a1ae
Paulo Rodrigues <paulovinicius356@gmail.com>
2024-11-05 11:34:51 -0300

    att cases

--- src/app/(pages)/cases/_components/cases.tsx
+++ src/app/(pages)/cases/_components/cases.tsx
@@ -12,16 +12,37 @@ export default function CaseSection() {
       name: "Us Clean Cosmetics",
       img: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Design%20sem%20nome%20(13)-FagNFxkgMzj5yn4esWntS8DFCyqJrV.png",
       href: "https://www.instagram.com/uscleancosmetics",
+      type: "Skin Care",
     },
     {
       name: "Nutty Friends",
       img: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Design%20sem%20nome%20(12)-8UXco7UfV8SM7cJQUjbzlS4Yi81dsR.png",
       href: "https://www.instagram.com/nuttyfriends/",
+      type: "Pastas de Amendoim",
     },
     {
       name: "Megga Zavatti",
       img: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/zavatti-wvzeNYzEZrTWnfNa86xayTWQ9QpuqH.png",
       href: "https://www.instagram.com/meggazavattimarmores/",
+      type: "Mármores e Granitos",
+    },
+    {
+      name: "Encaixa.ai",
+      img: "case-encaixa-ai.png",
+      href: "https://www.encaixa.ai/",
+      type: "Plataforma SaaS",
+    },
+    {
+      name: "La Ganexa",
+      img: "/case-laganexa.png",
+      href: "https://www.lojalaganexa.com.br/",
+      type: "Pasta de Amendoim",
+    },
+    {
+      name: "Agora Facilita",
+      img: "/case-agora-facilita.png",
+      href: "https://www.agorafacilita.com/",
+      type: "Ecommerce",
     },
   ]
 
@@ -32,19 +53,22 @@ export default function CaseSection() {
     <section className="bg-black py-16 md:py-24">
       <div className="container min-h-screen mt-32 mx-auto px-4 md:px-6">
         <h2 className="text-2xl md:text-4xl font-bold text-white mb-12">
-          Quem confia na Big Deal!
+          Clientes com quem Trabalhamos:
         </h2>
         
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
       {cases.map((item) => (
         <Card key={item.name} className="overflow-hidden">
           <CardContent className="p-0">
-            <div className="aspect-video relative overflow-hidden">
+            <div className="aspect-video relative overflow-hidden group">
               <img
-                src={item.img}
-                alt={`${item.name} case study`}
-                className="object-cover w-full h-full hover:scale-105 transition-transform duration-300"
+              src={item.img}
+              alt={`${item.name} case study`}
+              className="object-cover w-full h-full transition-transform duration-300 group-hover:scale-105"
               />
+                <div className="absolute inset-0 bg-gray-950 opacity-0 group-hover:opacity-50 transition-opacity duration-300 flex items-center justify-center">
+                <span className="text-white text-lg font-semibold">{item.type}</span>
+                </div>
             </div>
           </CardContent>
           <CardFooter className="flex items-center justify-between p-6">
