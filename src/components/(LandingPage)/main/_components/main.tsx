a498a696db7ba967dd22e904d9320bfb6cfb90aa
Paulo Rodrigues <paulovinicius356@gmail.com>
2024-11-06 13:40:32 -0300

    add video in main section na home e quem somos

--- src/components/(LandingPage)/main/_components/main.tsx
+++ src/components/(LandingPage)/main/_components/main.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import { ArrowUpRight } from "lucide-react";
 import { FlipWords } from "@/components/ui/flip-words";
-import GridPattern from "@/components/ui/grid-pattern";
-import { cn } from "@/lib/utils";
 
 export default function Main() {
   const words = ["Branding", "Social Media", "Loja Virtual", "Sites Profissionais"];
@@ -10,14 +8,18 @@ export default function Main() {
   return (
     <div>
       <section className="relative min-h-screen w-full bg-black px-4 py-12 text-white md:px-6 lg:px-8 flex items-center">
-        <GridPattern
-          width={20}
-          height={20}
-          x={-1}
-          y={-1}
-          className={cn("[mask-image:linear-gradient(to_bottom_right,white,transparent,white)]")}
-        />
-        <div className="container px-4 mx-auto">
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute inset-0 w-full grayscale h-full object-cover z-0 opacity-40"
+        >
+          <source src="/video-home.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="absolute inset-0 w-full h-full bg-gradient-to-r from-black opacity-100 z-10"></div>
+
+        <div className="container px-4 mx-auto z-20">
           <div className="absolute right-8 z-10 md:block">
             <div className="absolute inset-0 ml-[-6.6rem] mt-[31.5rem] lg:mt-14 md:ml-[-28rem] animate-spin-slow">
               {/* <Rotate /> */}
@@ -33,7 +35,7 @@ export default function Main() {
                   <span className="absolute bottom-0 md:bottom-0 lg:bottom-2 left-0 h-[2px] w-full bg-[#AEFF00]"></span>
                 </span>
               </h1>
-              <h2 className="text-base sm:text-lg md:text-lg">
+              <h2 className="text-base sm:text-lg md:text-lg ">
                 Soluções para
                 <FlipWords words={words} className="text-base sm:text-lg md:text-lg" />
               </h2>
--- src/components/(QuemSomos)/qmain/_components/qmain.tsx
+++ src/components/(QuemSomos)/qmain/_components/qmain.tsx
@@ -1,60 +1,57 @@
 import Link from "next/link"
 import { ArrowUpRight } from "lucide-react"
 import { FlipWords } from "@/components/ui/flip-words"
-import GridPattern from "@/components/ui/grid-pattern"
-import { cn } from "@/lib/utils"
 
 export default function QMain() {
-        const words = ["Descubra o Que Nos Torna Únicos!", "Venha Conhecer Nossa Abordagem!", "Explore Nossa Missão e Visão!"]
-    
-        return (
-            <div>
-                <section className="relative min-h-screen w-full bg-black px-4 py-12 text-white md:px-6 lg:px-8 flex items-center justify-center">
-                    <GridPattern
-                        width={20}
-                        height={20}
-                        x={-1}
-                        y={-1}
-                        className={cn(
-                            "[mask-image:linear-gradient(to_bottom_right,white,transparent,white)] ",
-                        )}
-                    />
-                    <div className="container mt-12 px-4 mx-auto">
-                        {/* Top right explore button */}
-                        <div className="absolute right-8  z-10  md:block">
-                            <div className="absolute inset-0 ml-[-6.6rem] mt-[31.5rem] lg:mt-14 md:ml-[-28rem] animate-spin-slow">
-                                {/* <Rotate /> */}
-                            </div>
-                        </div>
-    
-                        <div className="grid gap-8 md:grid-cols-2 md:gap-12">
-                            {/* Left content */}
-                            <div className="space-y-6 z-10">
-                                <h1 className="text-2xl font-bold text-[#AEFF00] leading-tight md:text-lg lg:text-4xl">
-                                    WE THINK BIG! 
-                                </h1>
-                                <p className="text-lg text-white leading-relaxed">
-                                Somos especializados em fornecer soluções de ponta e sob medida para suas necessidades. Não importa o que você esteja procurando, nós temos a solução.
-                                </p>
-                                <p className="text-lg text-white leading-relaxed">
-                                Na intersecção entre visão e execução, trazemos ideias criativas e uma equipe que transforma conceitos em narrativas atraentes.
-                                </p>
-                                <FlipWords words={words} className="text-base md:text-lg lg:text-lg" />
-                                
-                                <div className="flex flex-wrap gap-4">
-                                    <Link
-                                        href="https://api.whatsapp.com/send/?phone=5511953843871" target="_blank"
-                                        className="rounded-full flex border border-white/20 bg-transparent px-8 py-3 text-base font-semibold text-white transition-colors hover:bg-white/10"
-                                    >
-                                        QUERO UMA REUNIÃO
-                                        <ArrowUpRight className="w-6 h-6 text-white" />
-                                    </Link>
-                                </div>
-                            </div>
+    const words = ["Descubra o Que Nos Torna Únicos!", "Venha Conhecer Nossa Abordagem!", "Explore Nossa Missão e Visão!"]
+
+    return (
+        <div>
+            <section className="relative min-h-screen w-full bg-black px-4 py-12 text-white md:px-6 lg:px-8 flex items-center justify-center">
+            <video
+          autoPlay
+          loop
+          muted
+          className="absolute inset-0 w-full grayscale h-full object-cover z-0 opacity-40"
+        >
+          <source src="/video-quem-somos.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="absolute inset-0 w-full h-full bg-gradient-to-r from-black opacity-100 z-10"></div>
+
+                <div className="container mt-12 px-4 mx-auto">
+                    <div className="absolute right-8  z-10  md:block">
+                        <div className="absolute inset-0 ml-[-6.6rem] mt-[31.5rem] lg:mt-14 md:ml-[-28rem] animate-spin-slow">
                         </div>
                     </div>
-                </section>
-            </div>
-        )
-    }
-    
+
+                    <div className="grid gap-8 md:grid-cols-2 md:gap-12">
+                        <div className="space-y-6 z-10">
+                            <h1 className="text-2xl font-bold text-[#AEFF00] leading-tight md:text-lg lg:text-4xl">
+                                WE THINK BIG! 
+                            </h1>
+                            <p className="text-lg text-white leading-relaxed">
+                            Somos especializados em fornecer soluções de ponta e sob medida para suas necessidades. Não importa o que você esteja procurando, nós temos a solução.
+                            </p>
+                            <p className="text-lg text-white leading-relaxed">
+                            Na intersecção entre visão e execução, trazemos ideias criativas e uma equipe que transforma conceitos em narrativas atraentes.
+                            </p>
+                            <FlipWords words={words} className="text-base md:text-lg lg:text-lg" />
+                            
+                            <div className="flex flex-wrap gap-4">
+                                <Link
+                                    href="https://api.whatsapp.com/send/?phone=5511953843871" target="_blank"
+                                    className="rounded-full flex border border-white/20 bg-transparent px-8 py-3 text-base font-semibold text-white transition-colors hover:bg-white/10"
+                                >
+                                    QUERO UMA REUNIÃO
+                                    <ArrowUpRight className="w-6 h-6 text-white" />
+                                </Link>
+                            </div>
+                        </div>
+                        {/* Right content */}
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
